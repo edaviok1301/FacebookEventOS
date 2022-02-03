c6b13eb7fed08e0db4d97ab82e85e50327ae4738
edaviok1301 <eparedesba1301@gmail.com>
2022-02-03 15:44:41 -0500

    aeamano

--- src/android/FacebookEventOS.java
+++ src/android/FacebookEventOS.java
@@ -6,7 +6,8 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -14,19 +15,17 @@ public class FacebookEventOS extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+        if(action.equals("logSentFriendRequestEvent")){
+            this.logSentFriendRequestEvent(args.getString(0),callbackContext);
             return true;
         }
         return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    private void logSentFriendRequestEvent(String event,CallbackContext callbackContext){
+        AppEventsLogger logger = AppEventsLogger.newLogger(cordova.getContext());
+        logger.logEvent(event);
+        callbackContext.success();
     }
+
 }
--- www/FacebookEventOS.js
+++ www/FacebookEventOS.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'FacebookEventOS', 'coolMethod', [arg0]);
+exports.logSentFriendRequestEvent = function (arg0, success, error) {
+    exec(success, error, 'FacebookEventOS', 'logSentFriendRequestEvent', [arg0]);
 };
